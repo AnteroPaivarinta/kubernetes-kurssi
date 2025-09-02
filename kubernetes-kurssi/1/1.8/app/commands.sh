f37fa8cf752651a0d2e1c0a0d6cbedb2ce799b95
AnteroPaivarinta <antsapaivarinta@gmail.com>
2025-09-02 09:11:16 +0300

    1.8

--- kubernetes-kurssi/1/1.8/app/commands.sh
+++ kubernetes-kurssi/1/1.8/app/commands.sh
@@ -3,6 +3,6 @@ kubectl delete deployment --all
 k3d cluster create --port 8082:30080@agent:0 -p 8081:80@loadbalancer --agents 2
 k3d cluster start
 
-docker build  -no-cache -t anteropaivarinta/log-output-app:latest .
-docker push anteropaivarinta/log-output-app:latest
+docker build -no-cache -t anteropaivarinta/todo-app:latest .
+docker push anteropaivarinta/todo-app:latest
 
