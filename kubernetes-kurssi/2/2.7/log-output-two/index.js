ddba3bbb1a6b090f394b6bb31ab3d91a3b38480b
AnteroPaivarinta <antsapaivarinta@gmail.com>
2026-01-03 02:53:23 +0200

    2.7

--- kubernetes-kurssi/2/2.7/log-output-two/index.js
+++ kubernetes-kurssi/2/2.7/log-output-two/index.js
@@ -4,17 +4,19 @@ const path = require('path');
 require('dotenv').config();
 const message = process.env.MESSAGE;
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const pignsPath = process.env.PINGS_PATH;
+
 
 const logFile = path.join('/usr/src/app/files', 'log.txt');
 const another = path.join('/etc/config', 'information.txt');
 
 app.get('/', async(req, res) => {
-    
+    console.log("PATH", pignsPath, "PORT: "+port);
     try {
         const data = fs.readFileSync(logFile, 'utf8');
         const fileContent = fs.readFileSync(another, 'utf8');
-        const response = await fetch("http://ping-pong-app-svc:2345/pings");
+        const response = await fetch(pignsPath);
         const pingpongData = await response.json();
         console.log(data + ", Ping/Pong: "+pingpongData + "ENV: "+message, "fileContent: "+fileContent);
         res.send(data + ", Ping/Pong: "+pingpongData);
