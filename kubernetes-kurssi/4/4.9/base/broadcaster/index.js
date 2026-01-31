4cbd09c2e4788b4bb52b1391525317a92bc6a4af
AnteroPaivarinta <antsapaivarinta@gmail.com>
2026-01-31 03:56:40 +0200

    4.9

--- kubernetes-kurssi/4/4.9/base/broadcaster/index.js
+++ kubernetes-kurssi/4/4.9/base/broadcaster/index.js
@@ -4,26 +4,26 @@ const axios = require("axios");
 
 const NATS_URL = process.env.NATS_URL || "localhost:4222";
 const DISCORD_URL = process.env.DISCORD_TOKEN;
+const STATE = process.env.STATE || "unknown";
+//const STAGING = "STAGING";
+const PRODUCTION = "PRODUCTION";
 
 async function start() {
   const nc = await connect({ servers: NATS_URL });
   const jc = JSONCodec();
   
-  // TÄRKEÄÄ: Tämä queue varmistaa, ettei 6 replikaa lähetä duplikaatteja
   const sub = nc.subscribe("todo_updates", { queue: "broadcaster-group" });
 
   console.log("Broadcaster erillinen palvelu käynnissä...");
 
   for await (const m of sub) {
     const data = jc.decode(m.data);
-    
-    // Lähetys Discordiin
-    if (DISCORD_URL) {
+    console.log(`Viesti välitetty eteenpäin: ${data.message}`);
+    if (DISCORD_URL && STATE == PRODUCTION) {
       await axios.post(DISCORD_URL, {
         content: `Uusi todo tapahtuma: ${data.message}`
       });
     }
-    console.log(`Viesti välitetty eteenpäin: ${data.message}`);
   }
 }
 
